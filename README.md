0fc8f893a27cf8407da25fd7b2d7ffad36481cb1
kelwinv <kelwin08@gmail.com>
2020-11-03 17:25:18 -0300

    finalizando versão web

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Watch the video](/assets/Screenshot_1.png)](/assets/appVideo.mp4)
+
+[![Watch the video](https://i.imgur.com/vKb2F1B.png)](https://youtu.be/vt5fpE0bzSY)
--- backend/src/Controllers/socketController.ts
+++ backend/src/Controllers/socketController.ts
@@ -1,5 +1,5 @@
 import socketIo from "socket.io";
-import { io } from '../server';
+import { io } from "../server";
 
 interface Props extends socketIo.Socket {
   userName: string;
@@ -14,20 +14,30 @@ interface mensagensBoxList {
 let numUserConnected = 0;
 let usersList = [];
 let messageId = 0;
-
+let messageString = 0;
+let MessageSpawn = false;
 
 const mensagensBoxList: mensagensBoxList[] = [];
 
 function joinInChat(socket: Props) {
-  io.in('chat').emit("get users", usersList);
+  io.in("chat").emit("get users", usersList);
 
   socket.emit("get messagens", mensagensBoxList);
   console.log(`${socket.userName} has connected on chat`);
 }
 
+function handleMessageSpawn() {
+  setInterval(() => {
+    messageString = 0;
+    MessageSpawn = false;
+  }, 4000);
+  if (messageString >= 3) {
+    MessageSpawn = true;
+  }
+}
+
 export default {
   connetion(socket: Props) {
-    let addedUser = false;
 
     numUserConnected++;
     console.log(`${numUserConnected} users online`);
@@ -38,33 +48,45 @@ export default {
         return;
       }
 
-      if (addedUser) return;
-
       socket.emit("login", { isAuth: true, userName });
       socket.join("chat");
 
-      addedUser = true;
       socket.userName = userName;
       usersList.push(userName);
 
-      socket.broadcast.emit("user joined", { userName });
+      socket.broadcast.emit("user joined", userName);
 
       joinInChat(socket);
     });
 
     socket.on("send message", ({ author, message }) => {
+      if (MessageSpawn) return;
       const MemessageBox = { author, message, id: messageId };
+      messageString++;
+      handleMessageSpawn();
 
       messageId++;
 
-      io.in('chat').emit("new message", MemessageBox);
+      io.in("chat").emit("new message", MemessageBox);
       mensagensBoxList.push(MemessageBox);
+      console.log(MemessageBox);
     });
+
+    socket.on("exit chat", () =>{
+      usersList = usersList.filter((user) => user !== socket.userName);
+      socket.leave('chat', () => {
+        io.to('chat').emit("get users", usersList);
+        socket.broadcast.emit("user disconnected", socket.userName);
+        console.log(`${socket.userName} disconnected on chat`)
+      });
+    })
 
     socket.on("disconnect", () => {
       numUserConnected--;
       usersList = usersList.filter((user) => user !== socket.userName);
-      io.in('chat').emit("get users", usersList);
+      io.in("chat").emit("get users", usersList);
+
+      socket.broadcast.emit("user disconnected", socket.userName);
 
       console.log(
         `user has disconnected to server, ${numUserConnected} users online`
